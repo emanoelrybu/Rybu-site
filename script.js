19471f0971a2d60440c83dc52042f3bd7ca59d56
Emanoel Rybu <emanoel.rybu@gmail.com>
2025-08-13 21:01:48 +0100

    1

---
+++ script.js
@@ -0,0 +1,5 @@
+// Script básico para capturar envio do formulário
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Mensagem enviada! Entraremos em contato em breve.");
+});
